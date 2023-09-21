d737aea0c268cbcc7f15f2824481eeb9e988bae8
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-09-21 22:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-const infiList = document.getElementById("infi-list");
+//your code here!
+const con = document.getElementById("infi-list");
 
-// Define the number of items to add on each scroll
-const itemsToAdd = 2;
-
-// Variable to keep track of the current item number
-let currentItemNumber = 10;
-
-// Function to add new list items
-function addListItems() {
-  for (let i = 0; i < itemsToAdd; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = "Item " + (++currentItemNumber);
-    infiList.appendChild(listItem);
-  }
+let itemNo = 1;
+const addList = (num)=>{
+	for(let i=1; i<= num; i++){
+		const ele = document.createElement("li");
+		ele.innerHTML = `Item ${itemNo}`;
+		con.append(ele);
+		itemNo++;
+	}
+}
+con.onscroll = ()=>{
+	if(Math.ceil(con.clientHeight + con.scrollTop) >= con.scrollHeight){
+		addList(2);
+	}
 }
 
-// Function to check if the user has reached the end of the list
-function isEndOfList() {
-  const scrollTop = infiList.scrollTop;
-  const scrollHeight = infiList.scrollHeight;
-  const clientHeight = infiList.clientHeight;
-
-  return scrollTop + clientHeight >= scrollHeight;
-}
-
-// Event listener for scroll event
-infiList.addEventListener("scroll", function () {
-  if (isEndOfList()) {
-    addListItems();
-  }
-});
-
-// Initially add list items
-addListItems();
 
 
-
-
-
-
-// Add initial list items
-for (let i = 0; i < 10; i++) {
-  const listItem = document.createElement("li");
-  listItem.textContent = "Item " + (i + 1);
-  infiList.appendChild(listItem);
-}
+addList(10);
